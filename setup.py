01da6ab666f9f06a471fb63fa7c47ec66e33fbfc
joesider9 <joesider9@gmail.com>
2022-10-25 16:41:46 +0300

    final commit setup

--- setup.py
+++
@@ -1,8 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name="CensusBureauClassifier",
-    version="0.0.0",
-    description="CI/CD using github actions, dvc data versioning, Heroku and Fastapi",
-    author="joesider",
-)
