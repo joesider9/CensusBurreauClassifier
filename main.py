aff876abb481d138b771c3bb503177d355c92bea
joesider9 <joesider9@gmail.com>
2022-10-25 17:49:28 +0300

    final commit with dvc pull

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ from pydantic import BaseModel, Field
 
 from src.ml.model import *
 
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
+
 
 class InputData(BaseModel):
     age: int = Field(example=60)
